c139d9eacc3f304c84369357038f2fd0cba064e9
SamuelMoraesF <samuel@samuelmoraes.com>
2014-11-26 16:58:43 -0200

    Node.js

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ var primitive = require('express');
 var app = primitive();
 var http = require('http').Server(app);
 
-ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-if (typeof ipaddress === "undefined") {
-  console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-  ipaddress = "127.0.0.1";
-};
-
 //var mongo = require('mongoskin');
 //var db = mongo.db("mongodb://localhost:27017/nodetest2", {native_parser:true});
 
